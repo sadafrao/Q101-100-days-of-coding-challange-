874f9f434c94bc282715aceee104923d8922fd13
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 15:55:37 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// Q101
+// this function generate arandom integer between 1 to 10
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+console.log(generateRandomNumber());
